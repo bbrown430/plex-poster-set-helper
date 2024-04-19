cecf6c1d7b4c29d656378fc56e69e54e2d3c54fd
Douw Jacobs <douw1.jacobs@gmail.com>
2024-04-19 13:41:24 +0200

    feat(plex-labels): add label to plex content when poster is updated

--- plex-poster-set-helper.py
+++ plex-poster-set-helper.py
@@ -11,6 +11,7 @@ import json
 
 LABEL_RATING_KEYS = {}
 
+
 def plex_setup():
     if os.path.exists("config.json"):
         try:
@@ -20,15 +21,19 @@ def plex_setup():
             tv_library = config["tv_library"]
             movie_library = config["movie_library"]
         except:
-            sys.exit("Error with config.json file. Please consult the readme.md.") 
+            sys.exit("Error with config.json file. Please consult the readme.md.")
         try:
             plex = PlexServer(base_url, token)
         except requests.exceptions.RequestException:
-            sys.exit('Unable to connect to Plex server. Please check the "base_url" in config.json, and consult the readme.md.')
+            sys.exit(
+                'Unable to connect to Plex server. Please check the "base_url" in config.json, and consult the readme.md.'
+            )
         except plexapi.exceptions.Unauthorized:
-            sys.exit('Invalid Plex token. Please check the "token" in config.json, and consult the readme.md.')
+            sys.exit(
+                'Invalid Plex token. Please check the "token" in config.json, and consult the readme.md.'
+            )
         if isinstance(tv_library, str):
-            tv_library = [tv_library] 
+            tv_library = [tv_library]
         elif not isinstance(tv_library, list):
             sys.exit("tv_library must be either a string or a list")
         tv = []
@@ -37,9 +42,11 @@ def plex_setup():
                 plex_tv = plex.library.section(tv_lib)
                 tv.append(plex_tv)
             except plexapi.exceptions.NotFound:
-                sys.exit(f'TV library named "{tv_lib}" not found. Please check the "tv_library" in config.json, and consult the readme.md.')        
+                sys.exit(
+                    f'TV library named "{tv_lib}" not found. Please check the "tv_library" in config.json, and consult the readme.md.'
+                )
         if isinstance(movie_library, str):
-            movie_library = [movie_library] 
+            movie_library = [movie_library]
         elif not isinstance(movie_library, list):
             sys.exit("movie_library must be either a string or a list")
         movies = []
@@ -48,25 +55,29 @@ def plex_setup():
                 plex_movie = plex.library.section(movie_lib)
                 movies.append(plex_movie)
             except plexapi.exceptions.NotFound:
-                sys.exit(f'Movie library named "{movie_lib}" not found. Please check the "movie_library" in config.json, and consult the readme.md.')
+                sys.exit(
+                    f'Movie library named "{movie_lib}" not found. Please check the "movie_library" in config.json, and consult the readme.md.'
+                )
         return tv, movies
     else:
-        sys.exit("No config.json file found. Please consult the readme.md.")   
+        sys.exit("No config.json file found. Please consult the readme.md.")
 
 
-def cook_soup(url):  
+def cook_soup(url):
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
     }
 
     response = requests.get(url, headers=headers)
 
-    if response.status_code == 200 or (response.status_code == 500 and "mediux.pro" in url):
-        soup = BeautifulSoup(response.text, 'html.parser')
+    if response.status_code == 200 or (
+        response.status_code == 500 and "mediux.pro" in url
+    ):
+        soup = BeautifulSoup(response.text, "html.parser")
         return soup
     else:
-        sys.exit(f"Failed to retrieve the page. Status code: {response.status_code}")    
-        
+        sys.exit(f"Failed to retrieve the page. Status code: {response.status_code}")
+
 
 def title_cleaner(string):
     if " (" in string:
@@ -75,19 +86,19 @@ def title_cleaner(string):
         title = string.split(" -")[0]
     else:
         title = string
-    
+
     title = title.strip()
-    
+
     return title
 
 
 def parse_string_to_dict(input_string):
-    input_string = input_string.replace("\\","")
+    input_string = input_string.replace("\\", "")
     input_string = input_string.replace("u0026", "&")
 
-    json_start_index = input_string.find('{')
-    json_end_index = input_string.rfind('}')
-    json_data = input_string[json_start_index:json_end_index+1]
+    json_start_index = input_string.find("{")
+    json_end_index = input_string.rfind("}")
+    json_data = input_string[json_start_index : json_end_index + 1]
 
     parsed_dict = json.loads(json_data)
     return parsed_dict
@@ -98,7 +109,14 @@ def add_label_rating_key(library_item):
     config = json.load(open("config.json"))
     append_label = config.get("append_label", None)
 
-    MEDIA_TYPES_PARENT_VALUES = {'movie': 1, 'show': 2, 'season': 2, 'episode': 2, 'album': 9, 'track': 9}
+    MEDIA_TYPES_PARENT_VALUES = {
+        "movie": 1,
+        "show": 2,
+        "season": 2,
+        "episode": 2,
+        "album": 9,
+        "track": 9,
+    }
 
     existing_section = LABEL_RATING_KEYS.get(library_item.librarySectionID, {})
 
@@ -108,9 +126,14 @@ def add_label_rating_key(library_item):
         if str(library_item.ratingKey) not in existing_keys:
             existing_keys += [str(library_item.ratingKey)]
 
-        existing_type = existing_section.get("type", MEDIA_TYPES_PARENT_VALUES[library_item.type])
+        existing_type = existing_section.get(
+            "type", MEDIA_TYPES_PARENT_VALUES[library_item.type]
+        )
 
-        LABEL_RATING_KEYS[library_item.librarySectionID] = {"keys": existing_keys, "type": existing_type}
+        LABEL_RATING_KEYS[library_item.librarySectionID] = {
+            "keys": existing_keys,
+            "type": existing_type,
+        }
 
 
 def find_in_library(library, poster):
@@ -140,7 +163,7 @@ def find_collection(library, poster):
         for plex_collection in movie_collections:
             if plex_collection.title == poster["title"]:
                 return plex_collection
-    if not found:   
+    if not found:
         print(f"{poster['title']} not found in Plex library.")
 
 
@@ -148,18 +171,21 @@ def update_plex_labels():
 
     config = json.load(open("config.json"))
 
-    headers = {'X-Plex-Token': config["token"]}
-    
+    headers = {"X-Plex-Token": config["token"]}
+
     if LABEL_RATING_KEYS:
         for key, item in LABEL_RATING_KEYS.items():
 
-            params = {'type': item["type"],
-                        'id': ','.join(item["keys"]),
-                        'label.locked': 1,
-                        'label[0].tag.tag': config["append_label"]
-                        }
-            
-            url = '{base_url}/library/sections/{section_id}/all'.format(base_url=config["base_url"], section_id=key)
+            params = {
+                "type": item["type"],
+                "id": ",".join(item["keys"]),
+                "label.locked": 1,
+                "label[0].tag.tag": config["append_label"],
+            }
+
+            url = "{base_url}/library/sections/{section_id}/all".format(
+                base_url=config["base_url"], section_id=key
+            )
             r = requests.put(url, headers=headers, params=params)
 
             if r.status_code == 200:
@@ -184,22 +210,32 @@ def upload_tv_poster(poster, tv):
             elif poster["season"] >= 1:
                 if poster["episode"] == "Cover":
                     upload_target = tv_show.season(poster["season"])
-                    print(f"Uploaded art for {poster['title']} - Season {poster['season']}.")
+                    print(
+                        f"Uploaded art for {poster['title']} - Season {poster['season']}."
+                    )
                 elif poster["episode"] is None:
                     upload_target = tv_show.season(poster["season"])
-                    print(f"Uploaded art for {poster['title']} - Season {poster['season']}.")
+                    print(
+                        f"Uploaded art for {poster['title']} - Season {poster['season']}."
+                    )
                 elif poster["episode"] is not None:
                     try:
-                        upload_target = tv_show.season(poster["season"]).episode(poster["episode"])
-                        print(f"Uploaded art for {poster['title']} - Season {poster['season']} Episode {poster['episode']}.")
+                        upload_target = tv_show.season(poster["season"]).episode(
+                            poster["episode"]
+                        )
+                        print(
+                            f"Uploaded art for {poster['title']} - Season {poster['season']} Episode {poster['episode']}."
+                        )
                     except:
-                        print(f"{poster['title']} - {poster['season']} Episode {poster['episode']} not found, skipping.")
+                        print(
+                            f"{poster['title']} - {poster['season']} Episode {poster['episode']} not found, skipping."
+                        )
             if poster["season"] == "Backdrop":
-                upload_target.uploadArt(url=poster['url'])
+                upload_target.uploadArt(url=poster["url"])
             else:
-                upload_target.uploadPoster(url=poster['url'])
+                upload_target.uploadPoster(url=poster["url"])
             if poster["source"] == "posterdb":
-                time.sleep(6) # too many requests prevention
+                time.sleep(6)  # too many requests prevention
         except:
             print(f"{poster['title']} - Season {poster['season']} not found, skipping.")
 
@@ -210,7 +246,7 @@ def upload_movie_poster(poster, movies):
         movie.uploadPoster(poster["url"])
         print(f'Uploaded art for {poster["title"]}.')
         if poster["source"] == "posterdb":
-            time.sleep(6) # too many requests prevention
+            time.sleep(6)  # too many requests prevention
 
 
 def upload_collection_poster(poster, movies):
@@ -219,18 +255,18 @@ def upload_collection_poster(poster, movies):
         collection.uploadPoster(poster["url"])
         print(f'Uploaded art for {poster["title"]}.')
         if poster["source"] == "posterdb":
-                time.sleep(6) # too many requests prevention
+            time.sleep(6)  # too many requests prevention
 
 
 def set_posters(url, tv, movies):
     movieposters, showposters, collectionposters = scrape(url)
-    
+
     for poster in collectionposters:
         upload_collection_poster(poster, movies)
-        
+
     for poster in movieposters:
         upload_movie_poster(poster, movies)
-        
+
     for poster in showposters:
         upload_tv_poster(poster, tv)
 
@@ -239,23 +275,27 @@ def scrape_posterdb(soup):
     movieposters = []
     showposters = []
     collectionposters = []
-    
+
     # find the poster grid
-    poster_div = soup.find('div', class_='row d-flex flex-wrap m-0 w-100 mx-n1 mt-n1')
+    poster_div = soup.find("div", class_="row d-flex flex-wrap m-0 w-100 mx-n1 mt-n1")
 
     # find all poster divs
-    posters = poster_div.find_all('div', class_='col-6 col-lg-2 p-1')
+    posters = poster_div.find_all("div", class_="col-6 col-lg-2 p-1")
 
     # loop through the poster divs
     for poster in posters:
         # get if poster is for a show or movie
-        media_type = poster.find('a', class_="text-white", attrs={'data-toggle': 'tooltip', 'data-placement': 'top'})['title']
+        media_type = poster.find(
+            "a",
+            class_="text-white",
+            attrs={"data-toggle": "tooltip", "data-placement": "top"},
+        )["title"]
         # get high resolution poster image
-        overlay_div = poster.find('div', class_='overlay')
-        poster_id = overlay_div.get('data-poster-id')
+        overlay_div = poster.find("div", class_="overlay")
+        poster_id = overlay_div.get("data-poster-id")
         poster_url = "https://theposterdb.com/api/assets/" + poster_id
         # get metadata
-        title_p = poster.find('p', class_='p-0 mb-1 text-break').string
+        title_p = poster.find("p", class_="p-0 mb-1 text-break").string
 
         if media_type == "Show":
             title = title_p.split(" (")[0]
@@ -263,7 +303,7 @@ def scrape_posterdb(soup):
                 year = int(title_p.split(" (")[1].split(")")[0])
             except:
                 year = None
-                
+
             if " - " in title_p:
                 split_season = title_p.split(" - ")[1]
                 if split_season == "Specials":
@@ -272,7 +312,7 @@ def scrape_posterdb(soup):
                     season = int(split_season.split(" ")[1])
             else:
                 season = "Cover"
-            
+
             showposter = {}
             showposter["title"] = title
             showposter["url"] = poster_url
@@ -289,50 +329,57 @@ def scrape_posterdb(soup):
             else:
                 title = title_split[0]
             year = title_split[-1].split(")")[0]
-                
+
             movieposter = {}
             movieposter["title"] = title
             movieposter["url"] = poster_url
             movieposter["year"] = int(year)
             movieposter["source"] = "posterdb"
             movieposters.append(movieposter)
-        
+
         elif media_type == "Collection":
             collectionposter = {}
             collectionposter["title"] = title_p
             collectionposter["url"] = poster_url
             collectionposter["source"] = "posterdb"
             collectionposters.append(collectionposter)
-    
+
     return movieposters, showposters, collectionposters
 
 
 def scrape_mediux(soup):
-    base_url = "https://mediux.pro/_next/image?url=https%3A%2F%2Fapi.mediux.pro%2Fassets%2F"
+    base_url = (
+        "https://mediux.pro/_next/image?url=https%3A%2F%2Fapi.mediux.pro%2Fassets%2F"
+    )
     quality_suffix = "&w=3840&q=80"
-    
-    scripts = soup.find_all('script')
-    
+
+    scripts = soup.find_all("script")
 
     media_type = None
     showposters = []
     movieposters = []
     collectionposters = []
-        
+
     for script in scripts:
-        if 'files' in script.text:
-            if 'title' in script.text:
-                if 'Set Link\\' not in script.text:
+        if "files" in script.text:
+            if "title" in script.text or "set_name" in script.text:
+                if "Set Link\\" not in script.text:
                     data_dict = parse_string_to_dict(script.text)
                     poster_data = data_dict["set"]["files"]
 
     for data in poster_data:
-        if data["show_id"] is not None or data["show_id_backdrop"] is not None or data["episode_id"] is not None or data["season_id"] is not None or data["show_id"] is not None:
+        if (
+            data["show_id"] is not None
+            or data["show_id_backdrop"] is not None
+            or data["episode_id"] is not None
+            or data["season_id"] is not None
+            or data["show_id"] is not None
+        ):
             media_type = "Show"
         else:
             media_type = "Movie"
-                    
-    for data in poster_data:        
+
+    for data in poster_data:
         if media_type == "Show":
 
             episodes = data_dict["set"]["show"]["seasons"]
@@ -345,15 +392,25 @@ def scrape_mediux(soup):
             if data["fileType"] == "title_card":
                 episode_id = data["episode_id"]["id"]
                 season = data["episode_id"]["season_id"]["season_number"]
-                season_data = [episode for episode in episodes if episode["season_number"] == season][0]
-                episode_data = [episode for episode in season_data["episodes"] if episode["id"] == episode_id][0]
+                season_data = [
+                    episode
+                    for episode in episodes
+                    if episode["season_number"] == season
+                ][0]
+                episode_data = [
+                    episode
+                    for episode in season_data["episodes"]
+                    if episode["id"] == episode_id
+                ][0]
                 episode = episode_data["episode_number"]
             elif data["fileType"] == "backdrop":
                 season = "Backdrop"
                 episode = None
             elif data["season_id"] is not None:
                 season_id = data["season_id"]["id"]
-                season_data = [episode for episode in episodes if episode["id"] == season_id][0]
+                season_data = [
+                    episode for episode in episodes if episode["id"] == season_id
+                ][0]
                 episode = "Cover"
                 season = season_data["season_number"]
                 title = season_data["name"]
@@ -370,15 +427,17 @@ def scrape_mediux(soup):
                 elif data_dict["set"]["collection"]:
                     movie_id = data["movie_id"]["id"]
                     movies = data_dict["set"]["collection"]["movies"]
-                    movie_data = [movie for movie in movies if movie["id"] == movie_id][0]
+                    movie_data = [movie for movie in movies if movie["id"] == movie_id][
+                        0
+                    ]
                     title = movie_data["title"]
                     year = int(movie_data["release_date"][:4])
             elif data["collection_id"]:
                 title = data_dict["set"]["collection"]["collection_name"]
-            
+
         image_stub = data["id"]
         poster_url = f"{base_url}{image_stub}{quality_suffix}"
-        
+
         if media_type == "Show":
             showposter = {}
             showposter["title"] = show_name
@@ -388,7 +447,7 @@ def scrape_mediux(soup):
             showposter["source"] = "mediux"
             showposter["year"] = year
             showposters.append(showposter)
-        
+
         elif media_type == "Movie":
             if "Collection" in title:
                 collectionposter = {}
@@ -396,7 +455,7 @@ def scrape_mediux(soup):
                 collectionposter["url"] = poster_url
                 collectionposter["source"] = "mediux"
                 collectionposters.append(collectionposter)
-            
+
             else:
                 movieposter = {}
                 movieposter["title"] = title
@@ -404,7 +463,7 @@ def scrape_mediux(soup):
                 movieposter["url"] = poster_url
                 movieposter["source"] = "mediux"
                 movieposters.append(movieposter)
-            
+
     return movieposters, showposters, collectionposters
 
 
@@ -416,22 +475,22 @@ def scrape(url):
         soup = cook_soup(url)
         return scrape_mediux(soup)
     else:
-        sys.exit("Poster set not found. Check the link you are inputting.")  
+        sys.exit("Poster set not found. Check the link you are inputting.")
 
 
 if __name__ == "__main__":
     tv, movies = plex_setup()
-    
+
     while True:
         user_input = input("Enter a ThePosterDB set (or user) or a MediUX set url: ")
-        
-        if user_input.lower() == 'stop':
+
+        if user_input.lower() == "stop":
             print("Stopping...")
             break
-        elif user_input.lower() == 'bulk':
+        elif user_input.lower() == "bulk":
             file_path = input("Enter the path to the .txt file: ")
             try:
-                with open(file_path, 'r') as file:
+                with open(file_path, "r") as file:
                     urls = file.readlines()
                 for url in urls:
                     url = url.strip()
